40a280b04af2925cd14c7da575f216462d87643f
Mykola Kyrylenko <bulletbest@gmail.com>
2020-07-30 01:36:31 +0300

    added new file

---
+++ JS/task-2.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Напиши функцию countProps(obj), считающую кол - во свойств в объекте.Функция возвращает число - количество свойств.
+
+const countProps = function (obj) {
+  // твой код
+  let keys = Object.keys(obj);
+  return keys.length;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
---
+++ JS/task-4.js
@@ -0,0 +1,36 @@
+"use strict";
+// Напиши функцию countTotalSalary(employees) принимающую объект зарплат.Функция считает общую сумму запрплаты работников и возращает ее.Каждое поле объекта, передаваемого в функцию, имеет вид "имя": "зарплата".
+
+const countTotalSalary = function (employees) {
+  // твой код
+  const values = Object.values(employees);
+  // console.log(values);
+
+  let totalValues = 0;
+  for (const value of values) {
+    // console.log(values);
+    totalValues += value;
+  }
+  return totalValues;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
